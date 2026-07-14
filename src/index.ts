451286402f7ad3500dc777c194fa31eab3b8c5e3
Naimullah Md Jakaria <jakaria64.nm@hotmail.com>
2026-07-14 07:12:15 +0600

    Update src index.ts for all cors

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
@@ -19,7 +19,6 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.use('/api', routes);
-
 app.use(notFound);
 app.use(errorHandler);
 
